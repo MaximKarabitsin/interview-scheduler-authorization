f2aa4538b149881173556453bed6cc418a09fbc6
MaximKarabitsin <MaximKarabitsin@users.noreply.github.com>
2019-11-16 22:42:48 +0400

    Create project

---
+++ src/test/java/com/netcracker/interviewschedulerauthorization/InterviewSchedulerAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.interviewschedulerauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSchedulerAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
